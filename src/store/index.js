e7697f134e9dd2da7af2fcd0f72e735c22496d93
Jane <582194643@qq.com>
2020-03-13 11:10:57 +0800

    登录模块-解决vuex默认值的问题

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,15 @@ import Vuex from 'vuex'
 // 注册vuex
 Vue.use(Vuex)
 // 导入操作本地存储的方法
-import {getLocal} from '@/utils/local.js';
+import { getLocal } from '@/utils/local.js';
 
 export default new Vuex.Store({
   state: {
-    token: getLocal('ttToken').token,
-    refresh_token: getLocal('ttToken').refresh_token
+    //利用逻辑运算符短路，解决vuex默认值的bug
+    // 第一次登录，左边式子为null，短路
+    // 登录后有了token值，左边式子为true，取右边式子结果
+    token: getLocal('ttToken') && getLocal('ttToken').token,
+    refresh_token: getLocal('ttToken') && getLocal('ttToken').refresh_token
   },
   mutations: {
     // 修改token值的方法
