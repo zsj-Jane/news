b4b07814fa3b2ef5e3e3b260c010ead204acc822
Jane <582194643@qq.com>
2020-03-13 09:39:35 +0800

    登录模块-响应拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,10 +14,10 @@ request.interceptors.request.use(function (config) {
 });
 // 添加响应拦截器
 request.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
-    return response;
+    // 正常响应触发的函数，参数：response就是响应体
+    return response.data;
 }, function (error) {
-    // 对响应错误做点什么
+    // 响应错误触发的函数
     return Promise.reject(error);
 });
 // 暴露请求对象
