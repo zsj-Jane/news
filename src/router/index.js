cabe0a9d98cf12862a6cf6ec282fffa5b89e45e8
Jane <582194643@qq.com>
2020-03-13 11:54:14 +0800

    layout页面布局+tabbar路由添加、跳转

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,40 @@ import VueRouter from 'vue-router'
 // 注册路由
 Vue.use(VueRouter)
 // 导入组件
-import home from '@/views/home/index.vue';
 import login from '@/views/login/index.vue';
+import layout from '@/views/layout/index.vue';
+import home from '@/views/home/index.vue';
+import faq  from '@/views/faq/';
+import video from '@/views/video/';
+import mine from '@/views/mine/';
+
 // 路由规则
 const routes = [
   {
-    path:'/home',
-    component:home
-  },
-  {
     path:'/login',
     component:login
+  },
+  {
+    path:'/layout',
+    component:layout,
+    children:[
+      {
+        path:'/home',
+        component:home
+      },
+      {
+        path:'/faq',
+        component:faq
+      },
+      {
+        path:'/video',
+        component:video
+      },
+      {
+        path:'/mine',
+        component:mine
+      }
+    ]
   },
 ]
 // 实例化路由对象
---
+++ src/views/layout/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 子路由出口 -->
+    <router-view></router-view>
+    <!-- tabbar -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o" to="/home">主页</van-tabbar-item>
+      <van-tabbar-item icon="comment-o" to="/faq">问答</van-tabbar-item>
+      <van-tabbar-item icon="tv-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/mine">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "layout",
+  data() {
+    return {
+      // 记录被选中的tab标签的索引
+      active: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
