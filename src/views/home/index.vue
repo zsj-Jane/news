0378fc92aa0f452e660e5736176d3bcb6bdc26e7
Jane <582194643@qq.com>
2020-03-14 13:50:31 +0800

    首页模块-不同频道响应不同数据，$set

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -18,9 +18,9 @@
     <!-- 频道部分 -->
     <van-tabs v-model="active" class="tabs">
       <van-tab v-for="(item, index) in channelList" :key="index" :name="item.id" :title="item.name">
-        <van-pull-refresh v-model="pullLoading" @refresh="onRefresh">
-          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <van-cell v-for="(item,index) in list" :key="index" :title="item.title" />
+        <van-pull-refresh v-model="item.pullLoading" @refresh="onRefresh(item)">
+          <van-list v-model="item.loading" :finished="item.finished" finished-text="没有更多了" @load="onLoad(item)">
+            <van-cell v-for="(item1,index1) in item.list" :key="index1" :title="item1.title" />
           </van-list>
         </van-pull-refresh>
       </van-tab>
@@ -37,27 +37,21 @@ export default {
   name: "home",
   data() {
     return {
+      // 搜索框绑定数据
       value: "",
+      // 当前频道的id
       active: "",
       // 频道数据
-      channelList: [],
-      // 控制下拉刷新的状态
-      pullLoading: false,
-      // 列表数据
-      list: [],
-      // 控制列表的刷新状态，为false会调用onLoad，为true则不调用
-      loading: false,
-      // 是否已经刷到底部
-      finished: false
+      channelList: []
     };
   },
   methods: {
     // 加载数据的方法
-    async onLoad() {
+    async onLoad(item) {
       // 获取频道新闻推荐列表数据
       let res = await articleList({
         // 频道id
-        channel_id: this.active,
+        channel_id: item.id,
         // 时间戳
         timestamp: Date.now(),
         // 是否包含置顶
@@ -65,21 +59,21 @@ export default {
       });
       console.log(res);
 
-      this.list.push(...res.data.results);
+      item.list.push(...res.data.results);
       // 加载一段数据，改成false的话，后面滚到最后一定会调用onLoad
       // 这个属性是控制加载状态的，为false就会再调用onLoad，为true就不调用
-      this.loading = false;
+      item.loading = false;
       // 如果数据超过100个就不加载了，显示没有更多数据了
-      if (this.list.length >= 100) {
-        this.finished = true;
+      if (item.list.length >= 100) {
+        item.finished = true;
       }
     },
     // 下拉刷新的方法
-    onRefresh() {
+    onRefresh(item) {
       // 只要往下拽就会触发，一旦触发，会自动把下拉状态给为true
-      console.log("下拉刷新了" + this.pullLoading);
+      console.log("下拉刷新了" + item.pullLoading);
       setTimeout(() => {
-        this.pullLoading = false;
+        item.pullLoading = false;
       }, 1000);
     }
   },
@@ -88,6 +82,19 @@ export default {
     let res = await channelList();
     // 保存频道数据
     this.channelList = res.data.channels;
+    // 拿到频道数据后，给每个频道数据添加(控制下拉刷新的状态、列表数据、控制列表的刷新状态、是否已经刷到底部)这四个属性
+    this.channelList.forEach(item => {
+      // 控制下拉刷新的状态
+      // 直接赋值，界面不会跟着响应，要用$set
+      // item.pullLoading = false;
+      this.$set(item,'pullLoading',false);
+      // 列表数据(新闻数据)
+      this.$set(item,'list',[]);
+      // 控制列表的刷新状态，为false会调用onLoad，为true则不调用
+      this.$set(item,'loading',false);
+      // 是否已经刷到底部
+      this.$set(item,'finished',false);
+    });
   }
 };
 </script>
