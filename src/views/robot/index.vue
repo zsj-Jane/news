43c7d4b672fc0ad193042e82f91f22227c213e14
Jane <582194643@qq.com>
2020-03-28 16:05:31 +0800

    我的模块-小智机器人(使用socket.io-client实现收发消息、内容显示、滚动到最后)

--- src/views/robot/index.vue
+++ src/views/robot/index.vue
@@ -3,9 +3,13 @@
     <!-- 导航栏 -->
     <van-nav-bar title="小智机器人" left-arrow @click-left="$router.back()" />
     <!-- 聊天面板 -->
-    <div class="chat-wrap">
+    <div class="chat-wrap" ref="chat">
       <div class="chat-item" v-for="(item, index) in list" :key="index" :class="{user:item.isMe}">
-        <img class="avatar" :src="item.isMe?$route.params.icon:'http://img2.imgtn.bdimg.com/it/u=3476053323,2840761167&fm=26&gp=0.jpg'" alt />
+        <img
+          class="avatar"
+          :src="item.isMe?$route.params.icon:'http://img2.imgtn.bdimg.com/it/u=3476053323,2840761167&fm=26&gp=0.jpg'"
+          alt
+        />
         <div class="content">{{item.content}}</div>
       </div>
     </div>
@@ -14,15 +18,22 @@
       class="write"
       v-model="msg"
       show-action
-      action-text="发送"
       left-icon
       shape="round"
       background="#f4f5f6"
-    />
+      @keydown.enter="send"
+      @cancel="send"
+    >
+      <template #action>
+        <div @click="send">发送</div>
+      </template>
+    </van-search>
   </div>
 </template>
 
 <script>
+// 导入socket-io客户端
+import io from "socket.io-client";
 export default {
   name: "robot",
   data() {
@@ -33,8 +44,55 @@ export default {
         { isMe: true, content: "你好" }
       ],
       // 消息输入框内容
-      msg: ""
+      msg: "",
+      socket: null
     };
+  },
+  methods: {
+    // 发消息
+    send() {
+      // 判断输入框内容是否为空，为空就返回
+      if (this.msg.trim() == "") {
+        return;
+      }
+      // 如果不为空，就把输入的内容发送给服务器
+      this.socket.emit("message", {
+        msg: this.msg,
+        timestamp: Date.now()
+      });
+      // 添加到聊天记录数组中
+      this.list.push({
+        isMe: true,
+        content: this.msg
+      });
+      // 在界面下一次更新之后在执行
+      this.$nextTick(() => {
+        // 让滚动条滚动到最后
+        this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+      });
+      // 清空消息输入框内容
+      this.msg = "";
+    }
+  },
+  created() {
+    // 连接服务器,需要携带token，网址传递
+    this.socket = io(
+      `http://ttapi.research.itcast.cn?token=${this.$store.state.token}`
+    );
+    // 接收服务器返回的消息
+    this.socket.on("message", data => {
+      console.log(data);
+      // 将返回的消息，添加到聊天记录数组中
+      this.list.push({
+        isMe: false,
+        content: data.msg
+      });
+      // 机器人回复后也要滚动到最后，在界面下一次更新之后在执行
+      this.$nextTick(() => {
+        // 让滚动条滚动到最后
+        this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+      });
+    });
   }
 };
 </script>
@@ -52,12 +110,20 @@ export default {
   }
   // 聊天面板样式
   .chat-wrap {
-    padding: 0 10px 10px;
-    margin-bottom: 55px;
+    // 固定的高度，如果没有高度高度不会滚，因为没有固定高度，
+    // 他就是内容撑开的高度，那么永远都不需要他能滚动
+    // 布局技巧，自动计算高度
+    // 给个固定定位，再给top和bottom，就会自动算出高度
+    position: fixed;
+    top: 44px;
+    bottom: 55px;
+    width: 100%;
+    overflow: auto;
     // 聊天每一项
     .chat-item {
       display: flex;
       margin-top: 20px;
+      margin-left: 10px;
       .avatar {
         width: 40px;
         height: 40px;
@@ -74,6 +140,7 @@ export default {
       }
       &.user {
         flex-direction: row-reverse;
+        margin-right: 10px;
         .content {
           margin-right: 10px;
           background-color: #9eea6a;
